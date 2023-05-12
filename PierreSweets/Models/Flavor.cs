c7933e7e95a312d937d815b36cab2aa0cb04b3f1
osbrown1 <socknot@live.com>
2023-05-12 11:23:58 -0700

    updated Models and Create.cshtml files to restrict empty form submissions

--- PierreSweets/Models/Flavor.cs
+++ PierreSweets/Models/Flavor.cs
@@ -8,7 +8,9 @@ namespace PierreSweets.Models
         public int FlavorId { get; set; }
 
         [Required(ErrorMessage = "Please enter a name.")]
+        [StringLength(100, MinimumLength = 1)]
         public string Name { get; set; }
         public List<TreatFlavor> JoinEntities { get; set; }
     }
 }
+
--- PierreSweets/Models/Treat.cs
+++ PierreSweets/Models/Treat.cs
@@ -6,10 +6,20 @@ namespace PierreSweets.Models
   public class Treat
   {
     public int TreatId { get; set; }
-    
+
     [Required(ErrorMessage = "Please enter a name")]
-    public string Name {get; set;}
-    public List<TreatFlavor> JoinEntities { get;}
+    [StringLength(100, MinimumLength = 1)]
+    public string Name { get; set; }
+
+    public List<TreatFlavor> JoinEntities { get; }
     public ApplicationUser User { get; set; }
   }
 }
+
+
+// public int TreatId { get; set; }
+    
+//     [Required(ErrorMessage = "Please enter a name")]
+//     public string Name {get; set;}
+//     public List<TreatFlavor> JoinEntities { get;}
+//     public ApplicationUser User { get; set; }
